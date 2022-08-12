f84d139f662188e8d293fb36f5d0d16eb3b33c9d
Apple <santos.delaft@gmail.com>
2022-08-12 11:19:41 -0500

    adds navbar wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Navbar from './Navbar.js'
+import Button from './Button.js'
 import './App.css';
+import PreviousMap from 'postcss/lib/previous-map';
 
 function App() {
   return (
-    <Navbar/>
+    <Navbar />
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,20 @@
 import reddit_logo from './reddit_logo.svg'
-function Navbar(){
+import Searchbar from './Searchbar.js'
+import Button from './Button.js'
+import Dropdown from './Dropdown.js'
+
+function Navbar(props){
   return(
-    <div className="container bg-white max-w-screen-xl">
-      <div className="w-14 flex">
-        <img src={reddit_logo} className="p-2"></img>
+    <div className="container bg-white max-w-screen-xl flex justify-around">
+      <div className="flex basis-1/2">
+        <img src={reddit_logo} className="p-2 w-14"></img>
         <span className="m-auto font-bold">Home</span>
       </div>
+        <Searchbar/>
+        <Button name="login"/>
+        <Button name="Sign in"/>
+        <Dropdown/>
+
     </div>
   );
 
